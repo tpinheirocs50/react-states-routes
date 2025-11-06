07d578defdfb1432c59224396e42d7fbbc3162c7
Tiago Pinheiro <tiago.oliveira.pinheiro@gmail.com>
2025-11-06 08:12:38 +0000

    first commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -20,7 +20,7 @@ export default function Homepage() {
             <h1>This is the homepage!</h1>
 
             // Here we just increment the "count" value using "setCount", each time we click. we also display the current "count" value in the button text 
-            // also, we're passing the value of count to the property isZero, that we're using in the Button component to style the component based on the count value           
+            // also, we're passing the value of count to the property isZero, that we're using in the Button component to style the component based on the count value     
             <Button isZero={!count} handleClick={() => setCount(count => count + 1)}>Counter: {count}</Button>
 
             // this component will make use of the callMyFunction function, where it passes it a string as an argument, which the function then logs to the console
